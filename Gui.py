6d129b53e55b0199c57c957d0dfec71ae9c96617
jujube <jujube109@163.com>
2022-07-18 15:11:42 +0800

    怎加ip地址输入

--- Gui.py
+++ Gui.py
@@ -5,23 +5,27 @@ from tkinter import messagebox
 #port1=''
 
 #run_shell=AdbShell()
-class get_port:
+class GuiGet:
     def __init__(self):
-        self.__port = None
+        self.__IPport = dict(ip=None,port=None)
         self.root_window = tk.Tk()
-        self.root_window.title('test')
+        self.root_window.title('ADB日志查看器')
         self.root_window.geometry('400x180')
         self.root_window.resizable(0, 0)
         # 创建input控件接收用户输入的端口号，并赋初始值
-        initial = tk.IntVar()
-        initial.set(62025)
-        self.input = tk.Entry(self.root_window,textvariable =initial )
-        self.input.grid(row=0, column=2, padx=10, pady=5,)
+        port_initial = tk.IntVar()
+        port_initial.set(62025)
+        ip_initial=tk.StringVar()
+        ip_initial.set("127.0.0.1")
+        self.port_input = tk.Entry(self.root_window,textvariable =port_initial )
+        self.port_input.grid(row=0, column=2, padx=10, pady=5,)
+        self.ip_input = tk.Entry(self.root_window, textvariable=ip_initial)
+        self.ip_input.grid(row=1, column=2, padx=10, pady=5, )
 
 
     # 内置验证方法，点击提交后触发，判断输入值是否为数字
     def chechk(self):
-        self.__port = self.input.get()
+        self.__port = self.port_input.get()
         flag = self.__port.isdigit()
 
         if flag:
@@ -31,19 +35,27 @@ class get_port:
 
         else:
             messagebox.showwarning('提示', "只能输入数字")
-            self.input.delete(0, tk.END)
+            self.port_input.delete(0, tk.END)
             return False
-    def input_key(self):
+    def input_Ip_port(self):
 
         #创建label控件
         lable=tk.Label(self.root_window,text='端口号: ')
         #grid方法为通过网格的形式设置布局
         lable.grid(row=0, column=1, padx=10, pady=5)
+        # 创建label控件
+        Iplable = tk.Label(self.root_window, text='ip地址: ')
+        # grid方法为通过网格的形式设置布局
+        Iplable.grid(row=1, column=1, padx=10, pady=5)
 
         button=tk.Button(self.root_window,text="提交",width=10,command=self.chechk)
         button.grid(row=0, column=3, sticky="w", padx=10, pady=5)
+
         self.root_window.mainloop()
-        return self.__port
+        self.__IPport["ip"]=self.ip_input.get()
+        self.__IPport["port"] = self.port_input.get()
+        return self.__IPport
+
     def close_window(self):
         messagebox.showwarning('提示', '连接失败，端口号错误或未找到')
         self.root_window.quit()
--- main.py
+++ main.py
@@ -1,56 +1,59 @@
 import subprocess
-from  Gui import  get_port
+from Gui import GuiGet
+
 
 class AdbShell:
     def execute_cmd(self):
-        #实例化窗口创建类，输入要连接的adb设备端口号，代码例子中的以夜神为例62025
-        gui=get_port()
-        port=gui.input_key()
-        #将gui窗口获取到的端口号与adb命令拼接
-        connect_str="adb connect 127.0.0.1:"+port
-        #获取命令的输出结果
+        # 实例化窗口创建类，输入要连接的adb设备端口号，代码例子中的以夜神为例62025
+        gui = GuiGet()
+        iplist = gui.input_Ip_port()
+        print(iplist)
+        address = iplist["ip"] + ":" + iplist["port"]
+        # 将gui窗口获取到的端口号与adb命令拼接
+        connect_str = "adb connect " + address
+        print(connect_str)
+        # 获取命令的输出结果
         try:
-            cmd_connect=subprocess.getoutput(connect_str)
+            cmd_connect = subprocess.getoutput(connect_str)
         except UnicodeDecodeError:
             gui.close_window()
             exit(0)
-        #flag标志用于判断连接命令是否成功连接
-        flag="connected" in cmd_connect or "successfully" in cmd_connect
-
+        # flag标志用于判断连接命令是否成功连接
+        flag = "connected" in cmd_connect or "successfully" in cmd_connect
 
         '''
         可以通过遍历命令结果的stdout输出属性获取yjt包的进程的pid，注意获取到的stdout信息是byte流。需要再通过遍历stdout字符转换
         
         也可以直接使用communicate
         '''
-        pack_pid=str()
-        #先判断flag是否为真，连接是否成功
+        pack_pid = str()
+        # 先判断flag是否为真，连接是否成功
         if flag:
-            cmd=subprocess.Popen("adb -s 127.0.0.1:62025 shell top -n 1| grep xx",stdin = subprocess.PIPE,stdout=subprocess.PIPE)
-            top_byte=cmd.communicate()
-            top_str=str(top_byte)
+            cmd = subprocess.Popen(f"adb -s {address} shell top -n 1| grep xx", stdin=subprocess.PIPE,
+                                   stdout=subprocess.PIPE)
+            top_byte = cmd.communicate()
+            top_str = str(top_byte)
             pack_pid = top_str.split(" ")[1]
             print(pack_pid)
             # time.sleep(1)
             # for line in cmd_yjt.stdout:
             #     yjt_pid=line
 
-        #flag为假直接关闭窗口退出程序
+        # flag为假直接关闭窗口退出程序
         else:
             gui.close_window()
             exit(0)
-        #再通过遍历stdout流
+        # 再通过遍历stdout流
 
         # yjt_top_str=str()
         # for i in yjt_pid:
         #   yjt_top_str=yjt_top_str+chr(i)
         # yjt_pid=yjt_top_str.split(" ")[1]
 
-
-        log_command=f"adb -s 127.0.0.1:62025 logcat --pid={pack_pid}"
-        #if yjt_pid:
-        cmd_yjt_log = subprocess.Popen(log_command,stdout = subprocess.PIPE)
-        #只要日志命令没有终止，就循环打印出没行新增的日志信息
+        log_command = f"adb -s {address} logcat --pid={pack_pid}"
+        # if yjt_pid:
+        cmd_yjt_log = subprocess.Popen(log_command, stdout=subprocess.PIPE)
+        # 只要日志命令没有终止，就循环打印出没行新增的日志信息
         try:
             while not cmd_yjt_log.poll():
                 line = cmd_yjt_log.stdout.readline()
@@ -62,6 +65,7 @@ class AdbShell:
         except  KeyboardInterrupt:
             exit(0)
 
+
 if __name__ == '__main__':
-    a=AdbShell()
+    a = AdbShell()
     a.execute_cmd()
